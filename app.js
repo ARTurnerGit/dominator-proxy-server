caaa6d6a02f5553a2e9247f10173da7a115e6d74
Alun Turner <alun.turner@googlemail.com>
2020-06-19 11:36:38 +0100

    change to the use .setHeader method

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
