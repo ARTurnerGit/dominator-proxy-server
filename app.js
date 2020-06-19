8a7449f1169c45327e9092a41e04df564cb91135
Alun Turner <alun.turner@googlemail.com>
2020-06-19 10:27:06 +0100

    setting up get request on /

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const axios = require("axios");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(JSON.stringify({ msg: "congratulations, you made it" }));
+});
+
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, console.log(`listening on ${PORT}...`));
