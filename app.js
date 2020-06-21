1faff02d597867dd03def5eaca91c8978efabdcd
Alun Turner <alun.turner@googlemail.com>
2020-06-21 17:46:09 +0100

    next step refactor into mvc format

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const { JSDOM } = require("jsdom");
 
 const app = express();
 
@@ -15,13 +16,39 @@ app.get("/:gameNumber", (req, res) => {
   axios
     .get(baseURL + gameNumber)
     .then((axiosResponse) => {
-      res.send(axiosResponse.data);
+      const { document } = new JSDOM(axiosResponse.data).window;
+
+      let mapStyle = document.getElementById("map").style;
+      let map = {
+        url: mapStyle.backgroundImage.slice(4, -1),
+        width: mapStyle.width,
+        height: mapStyle.height,
+      };
+
+      let territories = {};
+      document
+        .querySelector("#map")
+        .querySelectorAll("a")
+        .forEach((element) => {
+          let name = element.getAttribute("data-name");
+          let xpos = element.getAttribute("data-x");
+          let ypos = element.getAttribute("data-y");
+          territories[name] = {
+            owner: "",
+            territories: 3,
+            xpos,
+            ypos,
+            highlighted: false,
+          };
+        });
+
+      res.json({ territories, map });
     })
-    .catch((err) =>
+    .catch((err) => {
       res
         .status(500)
-        .send({ msg: "probably an issue with the proxy server", err })
-    );
+        .send({ msg: "probably an issue with the proxy server", err });
+    });
 });
 
 const PORT = process.env.PORT || 9090;
