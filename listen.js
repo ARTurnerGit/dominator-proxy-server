c7397e39d9bc266a5147f474c37831441015f863
Alun Turner <alun.turner@googlemail.com>
2020-06-22 15:55:32 +0100

    alter listen.js for heroku

--- listen.js
+++ listen.js
@@ -1,4 +1,5 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 9090;
-app.listen(PORT, console.log(`listening on ${PORT}...`));
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
